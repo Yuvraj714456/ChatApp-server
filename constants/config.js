21be6735731ca83e1e52c6fd1b13e894cdd2d00d
Yuvraj714456 <yuvrajsingh70123@gmail.com>
2025-07-15 12:53:18 +0530

    fix: cors

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,9 @@
 const corsOptions={
     origin:[process.env.CLIENT_URL,
             "http://localhost:5173",
-            "http://localhost:4173"],
+            "http://localhost:4173",
+            "https://chat-app-frontend-ochre-seven.vercel.app"
+        ],
     methods:["GET","POST","PUT","DELETE"],        
     credentials:true
 }
